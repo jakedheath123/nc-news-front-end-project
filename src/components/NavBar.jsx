a9ae793963be31de96a249fc3eaab22349feb04c
jakedheath123 <jakedheath@gmail.com>
2020-04-09 17:49:28 +0100

    added link to home path of /

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "@reach/router";
 
 const NavBar = () => {
   return (
     <nav>
       <ul>
-        <li>Home</li>
+        <Link to="/">
+          <li>Home</li>
+        </Link>
       </ul>
     </nav>
   );
