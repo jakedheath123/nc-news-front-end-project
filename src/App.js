e0373f8a0e32f20579458c8463df1ba9397e60a8
jakedheath123 <jakedheath@gmail.com>
2020-04-09 17:53:11 +0100

    added Rarticle list to app and wrapped in router with a path of /

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
+import { Router } from "@reach/router";
+import ArticleList from "./components/ArticleList";
 
 function App() {
   return (
     <div>
       <Header />
       <NavBar />
+      <Router>
+        <ArticleList path="/" />
+      </Router>
     </div>
   );
 }
