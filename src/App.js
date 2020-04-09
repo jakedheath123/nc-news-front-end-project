e1ff63b1c6dc9f4ff3b3feb905df57c65f9ca7d3
jakedheath123 <jakedheath@gmail.com>
2020-04-09 17:18:48 +0100

    added nav bar to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div>
       <Header />
+      <NavBar />
     </div>
   );
 }
