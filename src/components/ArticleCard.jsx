d6f556d878532c8681168558061dc7b71b973770
jakedheath123 <jakedheath@gmail.com>
2020-04-10 14:00:21 +0100

    made title of article a link to its own article

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "@reach/router";
 
 const ArticleCard = props => {
-  const { title, author } = props.article;
+  const { title, author, article_id } = props.article;
   return (
     <li>
-      <h2>{title}</h2>
+      <Link to={`/article/${article_id}`}>
+        <h2>{title}</h2>
+      </Link>
       <h3>Author: {author}</h3>
     </li>
   );
