4ff6d2f4bca27ac35bdf35decac7df1fe836d831
jakedheath123 <jakedheath@gmail.com>
2020-04-10 14:23:05 +0100

    refactored url to stop repeating the url used

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,13 @@
 const axios = require("axios");
 
+const request = axios.create({
+  baseURL: "https://nc-news-be-project.herokuapp.com/api"
+});
+
 const getAllArticles = () => {
-  return axios
-    .get("https://nc-news-be-project.herokuapp.com/api/articles")
-    .then(({ data: { articles } }) => {
-      return articles;
-    });
+  return request.get("/articles").then(({ data: { articles } }) => {
+    return articles;
+  });
 };
 
 module.exports = { getAllArticles };
