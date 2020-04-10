1811f321640700e7d2ef9ace7a17c70bff4a45a9
jakedheath123 <jakedheath@gmail.com>
2020-04-10 16:01:32 +0100

    added state with a key of input and empty string as value, added name and value properties to input box

--- src/components/SearchArticleByAuthor.jsx
+++ src/components/SearchArticleByAuthor.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 
 export default class SearchArticleByAuthor extends Component {
+  state = {
+    input: ""
+  };
   render() {
+    const { input } = this.state;
+
     return (
       <form>
         <label>
           Search By Author:
-          <input type="text" placeholder="Name" />
+          <input type="text" name="input" value={input} placeholder="Name" />
         </label>
       </form>
     );
