6c57319f2ffab576d2efdb3921350d80e3158157
jakedheath123 <jakedheath@gmail.com>
2020-04-10 16:27:41 +0100

    added published, comments and votes infor to article card

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import { Link } from "@reach/router";
 
 const ArticleCard = props => {
-  const { title, author, article_id } = props.article;
+  const {
+    title,
+    author,
+    article_id,
+    created_at,
+    comment_count,
+    votes
+  } = props.article;
   return (
     <li>
       <h3 className="hvr-underline-from-center">
@@ -11,6 +18,9 @@ const ArticleCard = props => {
         </Link>
       </h3>
       <h4>Author: {author}</h4>
+      <h5>Published: {created_at}</h5>
+      <h5>Comments: {comment_count}</h5>
+      <h5>Votes: {votes}</h5>
       <hr></hr>
     </li>
   );
