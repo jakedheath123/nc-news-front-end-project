6e5c200a125525825f1856c1707e443895fdf476
jakedheath123 <jakedheath@gmail.com>
2020-04-10 14:17:22 +0100

    removed hard coded data in state to replace with data from fetchAllArticles

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -4,26 +4,7 @@ import * as api from "../utils/api";
 
 class ArticleList extends Component {
   state = {
-    articles: [
-      {
-        article_id: 33,
-        author: "weegembump",
-        created_at: "2018-05-30T15:59:13.341Z",
-        title: "Seafood substitutions are increasing",
-        topic: "cooking",
-        votes: 0,
-        comment_count: "6"
-      },
-      {
-        article_id: 28,
-        author: "happyamy2016",
-        created_at: "2018-05-27T03:32:28.514Z",
-        title: "High Altitude Cooking",
-        topic: "cooking",
-        votes: 0,
-        comment_count: "5"
-      }
-    ]
+    articles: []
   };
 
   componentDidMount() {
