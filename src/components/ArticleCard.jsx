7a806086d4faf902b92d4feac751ff469703afe7
jakedheath123 <jakedheath@gmail.com>
2020-04-10 13:51:43 +0100

    added title and author for each article to display through props

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
-const ArticleCard = () => {
-  return <div></div>;
+const ArticleCard = props => {
+  const { title, author } = props.article;
+  return (
+    <li>
+      <h2>{title}</h2>
+      <h3>{author}</h3>
+    </li>
+  );
 };
 
 export default ArticleCard;
