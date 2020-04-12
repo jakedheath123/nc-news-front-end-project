243b4125a19ba2406a32c1b61fd2f348f53bebfc
jakedheath123 <jakedheath@gmail.com>
2020-04-12 15:19:53 +0100

    added function to invoke with article comments, imported api file

--- src/components/SingleArticleComments.jsx
+++ src/components/SingleArticleComments.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
+import * as api from "../utils/api";
 
 export default class SingleArticleComments extends Component {
   state = {
     comments: []
   };
+
+  componentDidMount() {
+    api.getSingleArticleComments();
+  }
+
   render() {
     return <div></div>;
   }
