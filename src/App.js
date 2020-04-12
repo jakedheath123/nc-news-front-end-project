11af333525dc8c1e40bd343c63d05cdad75122be
jakedheath123 <jakedheath@gmail.com>
2020-04-12 14:25:57 +0100

    imported single article component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import NavBar from "./components/NavBar";
 import { Router } from "@reach/router";
 import ArticleList from "./components/ArticleList";
+import SingleArticle from "./components/SingleArticle";
 
 function App() {
   return (
