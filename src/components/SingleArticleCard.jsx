92ee470858fbccdba4a7571964d28a4300898f0c
jakedheath123 <jakedheath@gmail.com>
2020-04-12 15:01:52 +0100

    added line break to separate button from article data

--- src/components/SingleArticleCard.jsx
+++ src/components/SingleArticleCard.jsx
@@ -22,6 +22,7 @@ const SingleArticleCard = props => {
       <h4>Votes: {votes}</h4>
       <h4>Comment Count: {comment_count}</h4>
       <h4>Published: {dateConverter(created_at)}</h4>
+      <br></br>
       <Link to="/">
         <button>Back</button>
       </Link>
