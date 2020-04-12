2ba5472f516709d0030b0e0712340d443f2579da
jakedheath123 <jakedheath@gmail.com>
2020-04-12 14:57:18 +0100

    destrructured keys chosen from props, added values to individual article

--- src/components/SingleArticleCard.jsx
+++ src/components/SingleArticleCard.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
-const SingleArticleCard = () => {
-  return <div></div>;
+const SingleArticleCard = props => {
+  const {
+    title,
+    body,
+    created_at,
+    votes,
+    topic,
+    author,
+    comment_count
+  } = props.article;
+
+  return (
+    <article>
+      <h2>{title}</h2>
+      <h3>Written By: {author}</h3>
+      <p>{body}</p>
+      <h4>Topic: {topic}</h4>
+      <h4>Votes: {votes}</h4>
+      <h4>Comment Count: {comment_count}</h4>
+      <h4>Published: {created_at}</h4>
+    </article>
+  );
 };
 
 export default SingleArticleCard;
