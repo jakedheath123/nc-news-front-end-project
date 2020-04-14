af218b508c2a6216bfd9731315a9eb13de10c7fa
jakedheath123 <jakedheath@gmail.com>
2020-04-14 16:56:15 +0100

    re arranged information layout for comments

--- src/components/SingleArticleCommentsCard.jsx
+++ src/components/SingleArticleCommentsCard.jsx
@@ -5,10 +5,10 @@ const SingleArticleCommentsCard = props => {
   const { author, body, comment_id, votes, created_at } = props.comment;
   return (
     <>
-      <h4>{author}</h4>
-      <h4>{votes}</h4>
+      <p>{body}</p>
+      <h4>Written By: {author}</h4>
+      <h4>Votes: {votes}</h4>
       <h4>Posted: {dateConverter(created_at)}</h4>
-      <h4>{body}</h4>
       <button onClick={() => props.removeCommentFromList(comment_id)}>
         DELETE COMMENT
       </button>
