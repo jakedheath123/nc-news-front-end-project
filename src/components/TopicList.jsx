cb5b4f61c4da96ad9f19269a4a6b5de29dab5311
jakedheath123 <jakedheath@gmail.com>
2020-04-14 11:25:48 +0100

    added state with topics key and value of empty array to store data, and isLoading key and value of true ti display loading component until recieved

--- src/components/TopicList.jsx
+++ src/components/TopicList.jsx
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 
 export default class TopicList extends Component {
+  state = {
+    topics: [],
+    isLoading: true
+  };
+
   render() {
     return <div></div>;
   }
