bd66701f08eb00f7e6f5675d83c43b247cd67545
jakedheath123 <jakedheath@gmail.com>
2020-04-14 12:54:02 +0100

    updated name of article to be link to single article page, imported link from reach router

--- src/components/TopicCard.jsx
+++ src/components/TopicCard.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { Link } from "@reach/router";
 
 const TopicCard = props => {
-  const { title, created_at, votes, author, comment_count } = props.article;
+  const {
+    title,
+    created_at,
+    votes,
+    author,
+    comment_count,
+    article_id
+  } = props.article;
   return (
     <li>
-      <h2>{title}</h2>
+      <h2>
+        <Link to={`/article/${article_id}`}>{title}</Link>
+      </h2>
       <h3>Written By : {author}</h3>
       <h4>Votes : {votes}</h4>
       <h4>Comment Count : {comment_count}</h4>
