67df935191dea3c7fdd98ef431e7471219eefe99
jakedheath123 <jakedheath@gmail.com>
2020-04-14 11:28:10 +0100

    added component did mount with get all articles function invoked from api

--- src/components/TopicList.jsx
+++ src/components/TopicList.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
+import * as api from "../utils/api";
 
 export default class TopicList extends Component {
   state = {
     topics: [],
     isLoading: true
   };
+
+  componentDidMount() {
+    api.getAllArticles();
+  }
 
   render() {
     return <div></div>;
