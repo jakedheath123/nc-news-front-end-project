a28e86afeb60e6988d9d90f9337f9b0b07477d89
jakedheath123 <jakedheath@gmail.com>
2020-04-14 12:47:31 +0100

    inputted key values into a list format from keys of article

--- src/components/TopicCard.jsx
+++ src/components/TopicCard.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 
 const TopicCard = props => {
-  const {
-    title,
-    body,
-    created_at,
-    votes,
-    author,
-    comment_count
-  } = props.article;
-  return <div></div>;
+  const { title, created_at, votes, author, comment_count } = props.article;
+  return (
+    <li>
+      <h2>{title}</h2>
+      <h3>Written By : {author}</h3>
+      <h4>Votes : {votes}</h4>
+      <h4>Comment Count : {comment_count}</h4>
+      <h4>Published : {created_at}</h4>
+      <br></br>
+      <hr></hr>
+    </li>
+  );
 };
 
 export default TopicCard;
