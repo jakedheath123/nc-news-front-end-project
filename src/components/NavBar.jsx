50ebbc0759de4f01d7f8d79757c41db02076e1e2
jakedheath123 <jakedheath@gmail.com>
2020-04-14 11:00:07 +0100

    refactored to class component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,25 +1,25 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "@reach/router";
 
-const NavBar = () => {
-  return (
-    <nav className="main-nav">
-      <ul>
-        <Link className="link" to="/">
-          <li>Home</li>
-        </Link>
-        <Link className="link" to="/topics/football">
-          <li>Topic:Football</li>
-        </Link>
-        <Link className="link" to="/topics/coding">
-          <li>Topic:Coding</li>
-        </Link>
-        <Link className="link" to="/topics/cooking">
-          <li>Topic:Cooking</li>
-        </Link>
-      </ul>
-    </nav>
-  );
-};
-
-export default NavBar;
+export default class NavBar extends Component {
+  render() {
+    return (
+      <nav className="main-nav">
+        <ul>
+          <Link className="link" to="/">
+            <li>Home</li>
+          </Link>
+          <Link className="link" to="/topics/football">
+            <li>Topic:Football</li>
+          </Link>
+          <Link className="link" to="/topics/coding">
+            <li>Topic:Coding</li>
+          </Link>
+          <Link className="link" to="/topics/cooking">
+            <li>Topic:Cooking</li>
+          </Link>
+        </ul>
+      </nav>
+    );
+  }
+}
