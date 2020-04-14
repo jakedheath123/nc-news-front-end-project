af8c606083172fddacd588ddd1455b7ba89eeb0d
jakedheath123 <jakedheath@gmail.com>
2020-04-14 11:54:07 +0100

    destructured keys from props.article to be used to display for each article

--- src/components/TopicCard.jsx
+++ src/components/TopicCard.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 
-const TopicCard = () => {
+const TopicCard = props => {
+  const {
+    title,
+    body,
+    created_at,
+    votes,
+    author,
+    comment_count
+  } = props.article;
   return <div></div>;
 };
 
