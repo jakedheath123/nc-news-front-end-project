fd183ba6f69bb0cc447e0172b8e1b160779045d5
jakedheath123 <jakedheath@gmail.com>
2020-04-14 14:13:34 +0100

    destructured selected keys from props and displayed in h tags

--- src/components/SingleArticleCommentsCard.jsx
+++ src/components/SingleArticleCommentsCard.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
-const SingleArticleCommentsCard = () => {
-  return <div></div>;
+const SingleArticleCommentsCard = props => {
+  const { author, body, comment_id, votes } = props.comment;
+  return (
+    <>
+      <h4>{author}</h4>
+      <h4>{comment_id}</h4>
+      <h4>{votes}</h4>
+      <h4>{body}</h4>
+    </>
+  );
 };
 
 export default SingleArticleCommentsCard;
