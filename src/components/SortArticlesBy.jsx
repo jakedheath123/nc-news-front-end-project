3427e34c7f0f6bd03abe47a112edbb701559a898
jakedheath123 <jakedheath@gmail.com>
2020-04-14 17:10:21 +0100

    added form with options to sort by, added handle change function to onChange on selecting

--- src/components/SortArticlesBy.jsx
+++ src/components/SortArticlesBy.jsx
@@ -6,7 +6,27 @@ class SortArticlesBy extends Component {
   };
 
   render() {
-    return <div></div>;
+    return (
+      <>
+        <form>
+          <label>
+            Sort By:
+            <select onChange={this.handleChange} value={this.state.selection}>
+              <option></option>
+              <option name="dateCreated" value="created_at">
+                Published
+              </option>
+              <option name="commentCount" value="comment_count">
+                Comments
+              </option>
+              <option name="votes" value="votes">
+                Votes
+              </option>
+            </select>
+          </label>
+        </form>
+      </>
+    );
   }
 }
 
