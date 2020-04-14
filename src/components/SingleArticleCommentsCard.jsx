2e1ed5f4c96055bcc27e52e36e583bd42760c308
jakedheath123 <jakedheath@gmail.com>
2020-04-14 16:51:24 +0100

    added created at to comments info

--- src/components/SingleArticleCommentsCard.jsx
+++ src/components/SingleArticleCommentsCard.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
 const SingleArticleCommentsCard = props => {
-  const { author, body, comment_id, votes } = props.comment;
+  const { author, body, comment_id, votes, created_at } = props.comment;
   return (
     <>
       <h4>{author}</h4>
-      <h4>{comment_id}</h4>
       <h4>{votes}</h4>
+      <h4>Posted: {created_at}</h4>
       <h4>{body}</h4>
       <button onClick={() => props.removeCommentFromList(comment_id)}>
         DELETE COMMENT
