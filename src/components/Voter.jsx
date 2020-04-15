3528ef4f2fec90deec3b64af0cd3beb5d99a034c
jakedheath123 <jakedheath@gmail.com>
2020-04-15 12:41:56 +0100

    updated id key being passed from props

--- src/components/Voter.jsx
+++ src/components/Voter.jsx
@@ -12,7 +12,7 @@ class Voter extends Component {
         optimisticVotes: currentState.optimisticVotes + inc_votes
       };
     });
-    api.patchVotes(inc_votes, this.props.article_id, "articles");
+    api.patchVotes(inc_votes, this.props.id, "articles");
   };
 
   render() {
