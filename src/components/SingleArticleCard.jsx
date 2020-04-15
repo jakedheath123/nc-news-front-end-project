08ab3315a34a6170a72d845e81221657648bd0cf
jakedheath123 <jakedheath@gmail.com>
2020-04-15 12:01:48 +0100

    changed order of article content

--- src/components/SingleArticleCard.jsx
+++ src/components/SingleArticleCard.jsx
@@ -19,9 +19,9 @@ const SingleArticleCard = props => {
       <h3>Author: {author}</h3>
       <p>{body}</p>
       <h4>Topic: {topic}</h4>
-      <Voter votes={votes} />
       <h4>Comment Count: {comment_count}</h4>
       <h4>Published: {dateConverter(created_at)}</h4>
+      <Voter votes={votes} />
     </article>
   );
 };
