a449359373998ff1c94b2d0ddeb9ad6bb73a593a
jakedheath123 <jakedheath@gmail.com>
2020-04-15 11:56:23 +0100

    passed votes from artcile to voter component through props

--- src/components/SingleArticleCard.jsx
+++ src/components/SingleArticleCard.jsx
@@ -19,7 +19,7 @@ const SingleArticleCard = props => {
       <h3>Author: {author}</h3>
       <p>{body}</p>
       <h4>Topic: {topic}</h4>
-      <Voter />
+      <Voter votes={votes} />
       <h4>Comment Count: {comment_count}</h4>
       <h4>Published: {dateConverter(created_at)}</h4>
     </article>
