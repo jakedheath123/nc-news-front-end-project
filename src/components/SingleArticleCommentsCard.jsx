4982f21d24022fc8dbae130d225809dcba4c9ae7
jakedheath123 <jakedheath@gmail.com>
2020-04-15 12:45:26 +0100

    passed type key with value of comments to voter through props

--- src/components/SingleArticleCommentsCard.jsx
+++ src/components/SingleArticleCommentsCard.jsx
@@ -9,7 +9,7 @@ const SingleArticleCommentsCard = props => {
       <p>{body}</p>
       <h4>Written By: {author}</h4>
       <h4>Posted: {dateConverter(created_at)}</h4>
-      <Voter votes={votes} id={comment_id} />
+      <Voter votes={votes} id={comment_id} type={"comments"} />
       <button onClick={() => props.removeCommentFromList(comment_id)}>
         DELETE COMMENT
       </button>
