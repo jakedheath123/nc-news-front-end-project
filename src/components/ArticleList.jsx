aa5ecb8a428e80cd1ea035b0c73a428b2163d405
jakedheath123 <jakedheath@gmail.com>
2020-04-15 17:01:04 +0100

    added section element with images as content, imported images from images folder

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -4,6 +4,10 @@ import * as api from "../utils/api";
 import SearchArticleByAuthor from "./SearchArticleByAuthor";
 import SortArticlesBy from "./SortArticlesBy";
 import Loader from "./Loader";
+import Image1 from "../images/image1.jpg";
+import Image2 from "../images/image2.jpg";
+import Image3 from "../images/image3.jpg";
+import Image4 from "../images/image4.jpg";
 
 class ArticleList extends Component {
   state = {
@@ -26,6 +30,12 @@ class ArticleList extends Component {
         />
         <h2>Articles:</h2>
         <main className="home-container">
+          <section className="image-home">
+            <img src={Image1} alt="news"></img>
+            <img src={Image2} alt="news"></img>
+            <img src={Image3} alt="news"></img>
+            <img src={Image4} alt="news"></img>
+          </section>
           <ul className="articles">
             {articles.map(article => {
               return <ArticleCard key={article.article_id} article={article} />;
