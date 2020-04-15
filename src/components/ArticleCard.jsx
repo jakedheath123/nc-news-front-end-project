ead6562afa4663d75114df8662e4c54183deeaa9
jakedheath123 <jakedheath@gmail.com>
2020-04-15 16:43:13 +0100

    removed several h elements to just display title and author

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -3,14 +3,7 @@ import { Link } from "@reach/router";
 import { dateConverter } from "../utils/utils";
 
 const ArticleCard = props => {
-  const {
-    title,
-    author,
-    article_id,
-    created_at,
-    comment_count,
-    votes
-  } = props.article;
+  const { title, author, article_id } = props.article;
   return (
     <li>
       <h3 className="hvr-underline-from-center">
@@ -19,9 +12,6 @@ const ArticleCard = props => {
         </Link>
       </h3>
       <h4>Author: {author}</h4>
-      <h5>Published: {dateConverter(created_at)}</h5>
-      <h5>Comments: {comment_count}</h5>
-      <h5>Votes: {votes}</h5>
       <hr></hr>
     </li>
   );
