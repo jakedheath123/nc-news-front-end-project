8ebd064d7db21dd31b53e629e593c95fe8e7bd15
jakedheath123 <jakedheath@gmail.com>
2020-04-15 12:51:47 +0100

    updated third argument passed to patch votes function to use this.props.type to be a re usable component

--- src/components/Voter.jsx
+++ src/components/Voter.jsx
@@ -12,7 +12,7 @@ class Voter extends Component {
         optimisticVotes: currentState.optimisticVotes + inc_votes
       };
     });
-    api.patchVotes(inc_votes, this.props.id, "articles");
+    api.patchVotes(inc_votes, this.props.id, this.props.type);
   };
 
   render() {
