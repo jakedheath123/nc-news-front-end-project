f215f99fa10e58abbf8f85a5e6902710077ac30b
jakedheath123 <jakedheath@gmail.com>
2020-04-16 15:26:05 +0100

    removed list components from semantic ui , replace with p elements

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,10 @@
 import React from "react";
-import { List } from "semantic-ui-react";
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <List>
-        <List.Item>
-          <List.Content>Northcoders News</List.Content>
-        </List.Item>
-        <List.Item>
-          <List.Content>Leeds, U.K</List.Content>
-        </List.Item>
-      </List>
+      <p>Northcoders News</p>
+      <p>Leeds, UK</p>
     </footer>
   );
 };
