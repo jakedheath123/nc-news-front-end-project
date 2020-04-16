09d06978f27c06436926678718ab071d82dad2e6
jakedheath123 <jakedheath@gmail.com>
2020-04-16 13:43:44 +0100

    removed icons from footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,24 +6,10 @@ const Footer = () => {
     <footer className="footer">
       <List>
         <List.Item>
-          <List.Icon name="users" />
           <List.Content>Northcoders News</List.Content>
         </List.Item>
         <List.Item>
-          <List.Icon name="marker" />
           <List.Content>Leeds, U.K</List.Content>
-        </List.Item>
-        <List.Item>
-          <List.Icon name="mail" />
-          <List.Content>
-            <a href="mailto:news@news.com">ncnews@nc.com</a>
-          </List.Content>
-        </List.Item>
-        <List.Item>
-          <List.Icon name="linkify" />
-          <List.Content>
-            <a href="www.nc.com">northcoders-news.com</a>
-          </List.Content>
         </List.Item>
       </List>
     </footer>
