f5119851c244d14a4cc2b0d89e45e6d71cab1faf
jakedheath123 <jakedheath@gmail.com>
2020-04-16 11:45:12 +0100

    passed username in state down to header component through props

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ export default class App extends Component {
   render() {
     return (
       <div className="wrapper">
-        <Header />
+        <Header username={this.state.username} />
         <NavBar />
         <Router>
           <ArticleList path="/" />
