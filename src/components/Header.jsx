19a364a6c30241da80fcddaf9af0b434512def7b
jakedheath123 <jakedheath@gmail.com>
2020-04-16 11:48:42 +0100

    added username info from props to display logged in user below h1 element

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-const Header = () => {
+const Header = props => {
   return (
     <header>
       <h1>Northcoders News</h1>
+      <p>Logged in as: {props.username} </p>
     </header>
   );
 };
