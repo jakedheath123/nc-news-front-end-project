3c2d05791932d0638b5bcbf0bbc316172d5ee7c5
jakedheath123 <jakedheath@gmail.com>
2020-04-17 15:51:54 +0100

    refactored username

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
 const Header = props => {
+  const { username } = props;
   return (
     <header>
       <h1>Northcoders News</h1>
-      <p>Logged in as user : {props.username} </p>
+      <p>Logged in as user : {username} </p>
     </header>
   );
 };
