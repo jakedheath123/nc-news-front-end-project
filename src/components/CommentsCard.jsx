57a38c7bc8624ea5ca4a63eea036db738da9f768
jakedheath123 <jakedheath@gmail.com>
2020-04-17 16:31:47 +0100

    renamed single article comments card component

---
+++ src/components/CommentsCard.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { dateConverter } from "../utils/utils";
+import Voter from "./Voter";
+
+const SingleArticleCommentsCard = props => {
+  const { author, body, comment_id, votes, created_at } = props.comment;
+  const { removeCommentFromList, username } = props;
+  return (
+    <div>
+      <p>{body}</p>
+      <h4>Written By: {author}</h4>
+      <h4>Posted: {dateConverter(created_at)}</h4>
+      <Voter votes={votes} id={comment_id} type={"comments"} />
+      <br></br>
+      {author === username ? (
+        <button onClick={() => removeCommentFromList(comment_id)}>
+          DELETE Comment
+        </button>
+      ) : null}
+      <hr></hr>
+    </div>
+  );
+};
+
+export default SingleArticleCommentsCard;
